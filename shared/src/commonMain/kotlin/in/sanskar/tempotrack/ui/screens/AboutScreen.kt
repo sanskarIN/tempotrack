1014d9ad8c1abf07e91682ff153bbd12c8d914f2
Sanskar <sanskarin@outlook.in>
2026-08-19 15:09:28 +0530

    fix: escape Kotlin keyword in About screen

--- shared/src/commonMain/kotlin/in/sanskar/tempotrack/ui/screens/AboutScreen.kt
+++ shared/src/commonMain/kotlin/in/sanskar/tempotrack/ui/screens/AboutScreen.kt
@@ -1,4 +1,4 @@
-package in.sanskar.tempotrack.ui.screens
+package `in`.sanskar.tempotrack.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -19,20 +19,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
-import in.sanskar.tempotrack.resources.Res
-import in.sanskar.tempotrack.resources.about_bmc
-import in.sanskar.tempotrack.resources.about_business_gmail
-import in.sanskar.tempotrack.resources.about_business_outlook
-import in.sanskar.tempotrack.resources.about_github
-import in.sanskar.tempotrack.resources.about_license
-import in.sanskar.tempotrack.resources.about_privacy
-import in.sanskar.tempotrack.resources.about_support
-import in.sanskar.tempotrack.resources.about_tagline
-import in.sanskar.tempotrack.resources.about_version_platform
-import in.sanskar.tempotrack.resources.app_name
-import in.sanskar.tempotrack.resources.external_link_failed
-import in.sanskar.tempotrack.resources.made_by
-import in.sanskar.tempotrack.ui.openUriSafely
+import `in`.sanskar.tempotrack.resources.Res
+import `in`.sanskar.tempotrack.resources.about_bmc
+import `in`.sanskar.tempotrack.resources.about_business_gmail
+import `in`.sanskar.tempotrack.resources.about_business_outlook
+import `in`.sanskar.tempotrack.resources.about_github
+import `in`.sanskar.tempotrack.resources.about_license
+import `in`.sanskar.tempotrack.resources.about_privacy
+import `in`.sanskar.tempotrack.resources.about_support
+import `in`.sanskar.tempotrack.resources.about_tagline
+import `in`.sanskar.tempotrack.resources.about_version_platform
+import `in`.sanskar.tempotrack.resources.app_name
+import `in`.sanskar.tempotrack.resources.external_link_failed
+import `in`.sanskar.tempotrack.resources.made_by
+import `in`.sanskar.tempotrack.ui.openUriSafely
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
